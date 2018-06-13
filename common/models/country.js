e37ed0f33d81235728a209a25dd3bcbe37b90c3d
Burak <burak@Dilek-MacBook-Air.local>
2018-06-13 13:58:51 +0300

    html-pdf package added

--- common/models/country.js
+++ common/models/country.js
@@ -1,5 +1,28 @@
 'use strict';
 
-module.exports = function(Country) {
+var fs = require('fs');
+var pdf = require('html-pdf');
+
+module.exports = function (Country) {
+
+    Country.exportPdf = function (param, cb) {
+
+        var html = fs.readFileSync('./test.html', 'utf8');
+        var options = { format: 'Letter' };
+
+        pdf.create(html, options).toFile('./test.pdf', function (err, res) {
+            if (err) return console.log(err);
+            console.log(res); // { filename: '/app/businesscard.pdf' }
+            cb(null, res);
+        });
+    }
+
+    Country.remoteMethod("exportPdf", {
+        http: { path: '/exportPdf', verb: 'post', status: 200 },
+        accepts: [
+            { arg: 'param', type: 'object', http: { source: 'body' } }
+        ],
+        returns: { root: true, type: 'object' }
+    });
 
 };
